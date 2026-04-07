1d801dc1f2f657590d301cd392e9e7c8b40760bf
mathieudob8-jpg <mathieudob8@gmail.com>
2026-04-07 18:40:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function showPage(page) {
   const content = document.getElementById("content");
 
-  if (page === "home") {
+  else if (page === "home") {
     content.innerHTML = "<h1>Bienvenue sur Rift FA</h1><p>Choisissez une section.</p>";
   }
 
-  if (page === "general" || page === "streamer") {
+  else if (page === "general" || page === "streamer") {
   content.innerHTML = `<div class="logo-background"></div>`;
 }
 
-  if (page === "lspd") {
+  else if (page === "lspd") {
     content.innerHTML = "<h1>LSPD / Sheriff</h1><p>Règlement police.</p>";
   }
 
-  if (page === "ems") {
+  else if (page === "ems") {
     content.innerHTML = "<h1>EMS</h1><p>Règlement médical.</p>";
   }
 
-  if (page === "illegal") {
+  else if (page === "illegal") {
     content.innerHTML = "<h1>Illégal</h1><p>Contenu à venir...</p>";
   }
 }
