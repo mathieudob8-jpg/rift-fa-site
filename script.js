0caee245a4614bfe1ea3d03552afa8a3f3d78a4c
mathieudob8-jpg <mathieudob8@gmail.com>
2026-04-07 18:53:10 +0200

    Refactor showPage function and clean up CSS
    Refactored the showPage function to dynamically update content based on the selected page. Removed unused CSS styles for better clarity.

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-#content {
-  min-height: calc(100vh - 70px);
+function showPage(page) {
+  const content = document.getElementById("content");
+
+  if (page === "home") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>Bienvenue sur Rift FA</h1>
+        <p>Choisissez une section.</p>
+      </div>
+    `;
+  } else if (page === "general") {
+    content.innerHTML = `
+      <div class="page">
+        <img class="banner" src="meteor-logo-rift-roleplay-bgd.png" alt="Bannière Rift FA">
+        <h1>Règlement Général</h1>
+        <p>Ici tu pourras mettre tout le règlement général.</p>
+      </div>
+    `;
+  } else if (page === "streamer") {
+    content.innerHTML = `
+      <div class="page">
+        <img class="banner" src="meteor-logo-rift-roleplay-bgd.png" alt="Bannière Streamer">
+        <h1>Règlement Streamer</h1>
+        <p>Ici tu pourras mettre les règles stream.</p>
+      </div>
+    `;
+  } else if (page === "lspd") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>LSPD / Sheriff</h1>
+        <p>Règlement police.</p>
+      </div>
+    `;
+  } else if (page === "ems") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>EMS</h1>
+        <p>Règlement médical.</p>
+      </div>
+    `;
+  } else if (page === "illegal") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>Illégal</h1>
+        <p>Contenu à venir...</p>
+      </div>
+    `;
+  }
 }
 
-.page {
-  min-height: calc(100vh - 70px);
-}
-
-.logo-background {
-  width: 100%;
-  min-height: calc(100vh - 70px);
-  background-color: #020817; /* fallback */
-  background-image: url("meteor-logo-rift-roleplay-main.png");
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 300px;
-}
+showPage("home");
