bf4e794de4a67e5036120e6099e1dd6b88e93855
mathieudob8-jpg <mathieudob8@gmail.com>
2026-04-07 18:48:43 +0200

    Refactor showPage function to improve structure

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 function showPage(page) {
   const content = document.getElementById("content");
 
-  else if (page === "home") {
-    content.innerHTML = "<h1>Bienvenue sur Rift FA</h1><p>Choisissez une section.</p>";
-  }
-
-  else if (page === "general" || page === "streamer") {
-  content.innerHTML = `<div class="logo-background"></div>`;
-}
-
-  else if (page === "lspd") {
-    content.innerHTML = "<h1>LSPD / Sheriff</h1><p>Règlement police.</p>";
-  }
-
-  else if (page === "ems") {
-    content.innerHTML = "<h1>EMS</h1><p>Règlement médical.</p>";
-  }
-
-  else if (page === "illegal") {
-    content.innerHTML = "<h1>Illégal</h1><p>Contenu à venir...</p>";
+  if (page === "home") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>Bienvenue sur Rift FA</h1>
+        <p>Choisissez une section.</p>
+      </div>
+    `;
+  } else if (page === "general" || page === "streamer") {
+    content.innerHTML = `
+      <div class="page logo-background"></div>
+    `;
+  } else if (page === "lspd") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>LSPD / Sheriff</h1>
+        <p>Règlement police.</p>
+      </div>
+    `;
+  } else if (page === "ems") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>EMS</h1>
+        <p>Règlement médical.</p>
+      </div>
+    `;
+  } else if (page === "illegal") {
+    content.innerHTML = `
+      <div class="page">
+        <h1>Illégal</h1>
+        <p>Contenu à venir...</p>
+      </div>
+    `;
   }
 }
 
